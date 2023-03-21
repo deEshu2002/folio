95a9d32a7d6fdb3ee7e886b720b20d1988d8b16e
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-03-21 17:07:20 +0530

    better ux for work menu

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ function handleMenuVisibility(){
     const menu = document.getElementById("showcase-menu");
     const stickyTopValue = window.innerHeight/8;
 
-    if(menu!.getBoundingClientRect().top <=  stickyTopValue){
+    if(menu!.getBoundingClientRect().top <=  stickyTopValue + 80){
         menu!.animate({opacity: 1, zIndex:9}, { duration:1200, fill:'forwards'})
     }else{
         menu!.animate({opacity: 0, zIndex:-1}, { duration:1200, fill:'forwards'})
