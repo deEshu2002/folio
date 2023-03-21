f1c34af48d7ae88fba8fa385d8937b7207f5d909
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-03-21 17:02:09 +0530

    menu only visible when it hit top

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './style.css'
+
+
+function handleMenuVisibility(){
+    const menu = document.getElementById("showcase-menu");
+    const stickyTopValue = window.innerHeight/8;
+
+    if(menu!.getBoundingClientRect().top <=  stickyTopValue){
+        menu!.animate({opacity: 1, zIndex:9}, { duration:1200, fill:'forwards'})
+    }else{
+        menu!.animate({opacity: 0, zIndex:-1}, { duration:1200, fill:'forwards'})
+    }
+    
+}
+
+window.addEventListener('scroll', handleMenuVisibility, true);
