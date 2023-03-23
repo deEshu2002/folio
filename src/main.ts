c6f4348f1e7fcdc8157ef44610c51934ab2d43a7
Deepanshu <deepanshumehra2001@gmail.com>
2023-03-23 07:52:52 +0530

    added showcase menu transition on click

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import './style.css'
 
+const menu = document.getElementById("showcase-menu") as HTMLUListElement;
+const menuMiddleElement = document.getElementById('showcase-menu-middle') as HTMLLIElement;
+const menuValues = document.querySelectorAll('.showcase-item');
 
 function handleMenuVisibility(){
-    const menu = document.getElementById("showcase-menu");
     const stickyTopValue = window.innerHeight/8;
 
-    if(menu!.getBoundingClientRect().top <=  stickyTopValue + 80){
-        menu!.animate({opacity: 1, zIndex:9}, { duration:1200, fill:'forwards'})
+    if(menu.getBoundingClientRect().top <=  stickyTopValue + 80){
+        menu.animate({opacity: 1, zIndex:9}, { duration:600, fill:'forwards', easing:'cubic-bezier(0.25, 1, 0.5, 1)'})
     }else{
-        menu!.animate({opacity: 0, zIndex:-1}, { duration:1200, fill:'forwards'})
+        menu.animate({opacity: 0, zIndex:-1}, { duration:600, fill:'forwards', easing:'cubic-bezier(0.25, 1, 0.5, 1)'})
     }
     
 }
 
 window.addEventListener('scroll', handleMenuVisibility, true);
+
+
+function handleShowCaseMenuTransition(e:Event){
+    e.preventDefault();
+
+    const target =  (e.target as HTMLLIElement);
+    const currTop = target.getBoundingClientRect().top;
+    const middleTop = menuMiddleElement.getBoundingClientRect().top;
+    
+    const changeValue = (middleTop - currTop);
+    menuValues.forEach((elem) => {
+        elem.animate({ transform: `translateY(${changeValue}px)` }, {duration: 600, fill:'forwards', easing:'cubic-bezier(0.76, 0, 0.24, 1)'});
+    })
+}
+
+menuValues.forEach((item, idx) => {
+    item.addEventListener('click', (e) => handleShowCaseMenuTransition(e), false);
+})
