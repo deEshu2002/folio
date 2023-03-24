54cd28bea2e4a83cc1898852b28d9e82747e537e
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-03-24 08:28:25 +0530

    added rest folder structure

---
+++ src/NavOperations.ts
@@ -0,0 +1,11 @@
+export function openMenuModal(){
+    const target = document.querySelector('.circular-menu') as HTMLDivElement;
+    if(target.classList.contains('active')){
+        target.classList.remove('active');
+        (document.querySelector('body') as HTMLBodyElement).style.overflow = 'auto';
+    }
+    else{
+        target.classList.add('active');
+        (document.querySelector('body') as HTMLBodyElement).style.overflow = 'hidden';
+    }
+}
