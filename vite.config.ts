447d45990d79afe74824175fca3a4494da9be396
deeshu <deepanshumehra2001@gmail.com>
2024-01-13 21:44:40 +0530

    fixed pwa file location

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   plugins:[ VitePWA({
       registerType:'autoUpdate',
+      injectRegister: 'script-defer',
       manifest: {
         name: 'Deepanshu',
         short_name: 'DM',
@@ -22,52 +23,54 @@ export default defineConfig({
             "src": "/folio/android-chrome-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
-            "purpose": "any maskable"
+            "purpose": "any"
         },
         {
             "src": "/folio/android-chrome-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
-            "purpose": "any maskable"
+            "purpose": "any"
         }
         ],
         display: "standalone",
-        orientation:"portrait"
-      },
-          workbox:{
-            globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
-            globIgnores: [
-              "node_modules/**/*"
-            ],
-            runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365 
+        orientation:"portrait",
+        screenshots: [
+          {
+           "src": "/folio/ss1.png",
+            "sizes": "2048x1536",
+            "type": "image/png",
+            "form_factor": "wide",
+            "label": "Wider view of Portfolio of Software Developer based in India"
           },
-          cacheableResponse: {
-            statuses: [0, 200]
+          {
+            "src": "/folio/ssm1.png",
+            "type": "image/png",
+            "sizes": "720x1600",
+            "form_factor": "narrow",
+            "label": "Narrower view of Portfolio of Software Developer based in India"
+          },
+      ]
+      },
+      workbox:{
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+        globIgnores: [
+          "node_modules/**/*"
+        ],
+        runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'google-fonts-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365 
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
           }
-        }
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'gstatic-fonts-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365 
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          },
-        }
-      }
+        },
     ]
           },
           devOptions:{
@@ -78,6 +81,7 @@ export default defineConfig({
         
         ),
       ],
+      
       
   base: '/folio/'
 });
